0d70dd9aa36ff7ec2a551931527bb54f781e5a16
raghav0477 <sharmaraghav0412@gmail.com>
2023-01-06 14:01:27 +0530

    responsiveness fixed II

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-// const toggle = document.getElementById("toggle");
-// const navbar = document.getElementById("mob-nav");
-// const header = document.getElementById("header");
-// console.log(navbar);
-
-// document.onclick = function (e) {
-//   if (
-//     e.target.id !== "header" &&
-//     e.target.id !== "toggle" &&
-//     e.target.id !== "navbar"
-//   ) { 
-//     toggle.classList.remove("active");
-//     navbar.classList.remove("active");
-
-//     document.body.classList.toggle('lock-scroll');
-//   }
-// };
-// toggle.onclick = function () {
-//   toggle.classList.toggle("active");
-//   navbar.classList.toggle("active");
-// };
-// if (document.getElementById('#mob-nav').is(':visible')) {
-//   document.getElementById('.site-wrapper').addClass(".lock-scroll");
-// } else {
-//   document.querySelector('body').removeClass(".lock-scroll");
-// }
-// function lockScroll() {
-//   document.body.classList.toggle('lock-scroll');
-// }
 
 const add = document.querySelector(".mobile-nav-icon-menu");
 const mobile_nav = document.querySelector(".mobile-nav-menu")

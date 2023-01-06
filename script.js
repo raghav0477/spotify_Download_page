f6101386dd9726225dc82963dad94808c6c289d7
raghav0477 <sharmaraghav0412@gmail.com>
2023-01-06 16:35:27 +0530

    fixed a scroll issue

--- script.js
+++ script.js
@@ -2,13 +2,15 @@
 const add = document.querySelector(".mobile-nav-icon-menu");
 const mobile_nav = document.querySelector(".mobile-nav-menu")
 const close = document.querySelector(".mobile-nav-icon-close")
-
+const body = document.querySelector("body")
+console.log(body);
 const toogleNavbar = ()=>{
-  // alert("kldhfklh")
   mobile_nav.classList.add("active")
+  body.classList.add("fixed-position");
 }
 const removeClass = ()=>{
   mobile_nav.classList.remove("active")
+  body.classList.remove("fixed-position");
 }
 console.log(close);
 add.addEventListener('click', ()=> toogleNavbar())
